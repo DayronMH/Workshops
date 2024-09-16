73691638435dff6e8d31495d74fafed26e030b41
DayronMH <dayronmarenco123@gmail.com>
2024-09-16 13:57:28 -0600

    Php added

---
+++ workshop1/index.php
@@ -0,0 +1,9 @@
+<?php
+    date_default_timezone_set('America/Costa_Rica');
+    
+    $fechaActual = date('Y-m-d');  
+    $horaActual = date('H:i:s');   
+
+    echo "Fecha actual: " . $fechaActual . "<br>";
+    echo "Hora actual: " . $horaActual;
+?>
